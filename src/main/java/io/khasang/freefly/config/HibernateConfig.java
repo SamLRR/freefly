c1720f432f60d271b29f7de32225d1c55baa57fb
SamLRR <sergei.ivanov79@list.ru>
2018-07-30 15:00:45 +0300

    Add Correct Hibernate Core

--- src/main/java/io/khasang/freefly/config/HibernateConfig.java
+++ src/main/java/io/khasang/freefly/config/HibernateConfig.java
@@ -24,7 +24,7 @@ public class HibernateConfig {
     private Environment environment;
 
     @Bean
-    public DataSource dataSource(){
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(environment.getRequiredProperty("hibernate.Driver"));
         dataSource.setUrl(environment.getRequiredProperty("hibernate.url"));
@@ -32,37 +32,36 @@ public class HibernateConfig {
         dataSource.setPassword(environment.getRequiredProperty("hibernate.password"));
         return dataSource;
     }
+
     @Bean
-    public LocalSessionFactoryBean sessionFactory(){
+    public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("io.khasang.freefly.data");
+        sessionFactory.setPackagesToScan("io.khasang.freefly.entity");
         sessionFactory.setHibernateProperties(properties());
         return sessionFactory;
     }
 
     private Properties properties() {
         Properties properties = new Properties();
-        properties.put("hibernate.dialect",environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql",environment.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.format_sql",environment.getRequiredProperty("hibernate.format_sql"));
-        properties.put("hibernate.hbm2ddl.auto",environment.getRequiredProperty("hibernate.hbn2dll.auto"));
-        properties.put("show_sql",environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2dll.auto"));
+        properties.put("show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         return properties;
     }
 
     @Bean
     @Autowired
-    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory){
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
         HibernateTransactionManager manager = new HibernateTransactionManager();
         manager.setSessionFactory(sessionFactory);
         return manager;
     }
 
     @Bean
-    PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+    PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
         return new PersistenceExceptionTranslationPostProcessor();
     }
-
-
 }
